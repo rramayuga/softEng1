28c1bdd9a93ed826c3e5b5ba32673218a6acded3
rramayuga <58220050+rramayuga@users.noreply.github.com>
2023-09-05 23:05:01 +0800

    push

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello{
   public static void main(String []args){
     System.out.println("Hello, Software Engineering Course!");
-    System.out.println("My name is MAYUGA, Ronne Rae!")
+    System.out.println("My name is MAYUGA, Ronne Rae!");
+    System.out.println ("Complicated world of SoftDev that is why it pays much!");
       }
 }
